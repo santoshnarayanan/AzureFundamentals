af375efbf0214b28b61e2c331122a60a8004d403
skn33 <skn33@LAPTOP-3288926P>
2022-02-06 17:28:42 +0530

    Add project files.

---
+++ AzureBlobProject/Services/ContainerService.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzureBlobProject.Services
+{
+    public class ContainerService : IContainerService
+    {
+        private readonly BlobServiceClient _blobClient;
+
+        public ContainerService(BlobServiceClient blobClient)
+        {
+            _blobClient = blobClient;
+        }
+
+        public async Task CreateContainer(string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            await blobContainerClient.CreateIfNotExistsAsync(PublicAccessType.BlobContainer);
+        }
+
+        public async Task DeleteContainer(string containerName)
+        {
+            BlobContainerClient blobContainerClient = _blobClient.GetBlobContainerClient(containerName);
+            await blobContainerClient.DeleteIfExistsAsync();
+        }
+
+        public async Task<List<string>> GetAllContainer()
+        {
+            List<string> containerName = new();
+            await foreach(BlobContainerItem blobContainerItem in _blobClient.GetBlobContainersAsync())
+            {
+                containerName.Add(blobContainerItem.Name);
+            }
+
+            return containerName;
+        }
+
+        public Task<List<string>> GetAllContainerAndBlobs()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
