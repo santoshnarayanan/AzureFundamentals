5c775ea1bd8565766e2a928d352fdaee78b162ee
santosh33 <santosh33@gmail.com>
2022-02-11 02:46:58 +0530

    SAS Token generated using .Net

--- AzureBlobProject/Controllers/HomeController.cs
+++ AzureBlobProject/Controllers/HomeController.cs
@@ -8,10 +8,12 @@ namespace AzureBlobProject.Controllers
     public class HomeController : Controller
     {
         private readonly IContainerService _containerService;
+        private readonly IBlobService _blobService;
 
-        public HomeController(IContainerService containerService)
+        public HomeController(IContainerService containerService, IBlobService blobService)
         {
             _containerService = containerService;
+            _blobService = blobService;
         }
 
         public async Task<IActionResult> Index()
@@ -19,9 +21,9 @@ namespace AzureBlobProject.Controllers
             return View(await _containerService.GetAllContainerAndBlobs());
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> Images()
         {
-            return View();
+            return View(await _blobService.GetAllBlobsWithUri("privatecontainer"));
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
