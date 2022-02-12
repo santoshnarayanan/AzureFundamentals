3782f292b70eb7373b589ca9c029857a0dcaa5b6
santosh33 <santosh33@gmail.com>
2022-02-13 01:29:59 +0530

    Read Azure messages and inserting into SQL database

--- AzureTangyFunc/OnQueueTriggerUpdateDatabase.cs
+++ AzureTangyFunc/OnQueueTriggerUpdateDatabase.cs
@@ -1,4 +1,6 @@
 using System;
+using AzureTangyFunc.Data;
+using AzureTangyFunc.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -7,10 +9,21 @@ namespace AzureTangyFunc
 {
     public class OnQueueTriggerUpdateDatabase
     {
+        private readonly AzureTangyDbContext _db;
+
+        public OnQueueTriggerUpdateDatabase(AzureTangyDbContext db)
+        {
+            _db = db;
+        }
+
         [FunctionName("OnQueueTriggerUpdateDatabase")]
-        public void Run([QueueTrigger("SalesRequestInBound", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
+        public void Run([QueueTrigger("SalesRequestInBound", Connection = "AzureWebJobsStorage")]SalesRequest myQueueItem, ILogger log)
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+
+            myQueueItem.Status = "Submitted";
+            _db.SalesRequests.Add(myQueueItem);
+            _db.SaveChanges();
         }
     }
 }
